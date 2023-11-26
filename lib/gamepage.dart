c62e6bce3ad1d479dd7fd3ae30b5af8052d3415f
landrescaballero <cleonard@uninorte.edu.co>
2023-11-26 15:19:17 -0500

    juego completamente funcional player vs pc

--- lib/gamepage.dart
+++ lib/gamepage.dart
@@ -10,10 +10,27 @@ class GamePage extends StatefulWidget {
 }
 
 class _GamePageState extends State<GamePage> {
-  Battle game = Battle();
-  var data = "Your action";
+  late Battle game;
+  var data = 0;
+  bool isTurnOver = true;
+  @override
+  void initState() {
+    super.initState();
+    game = Battle(onTurnOver: (bool value) {
+      setState(() {
+        isTurnOver = value;
+        debugPrint("tappable to $value");
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    Size screenSize = MediaQuery.of(context).size;
+    double max = screenSize.height / 2 - 104.0;
+    double alto = max - ((max * 33 / 70 - 10) * 3 - max);
+    double ancho = alto * 33 / 70;
+
     return Scaffold(
       appBar: AppBar(title: const Text("Fight Warriors!"), actions: const [
         IconButton(icon: Icon(Icons.exit_to_app), onPressed: null),
@@ -21,58 +38,84 @@ class _GamePageState extends State<GamePage> {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Expanded(flex: 1, child: GameWidget(game: game)),
+          Expanded(
+            flex: 1,
+            child: GestureDetector(
+              onTap: () {
+                setState(() {
+                  data = 4;
+                  //game.playerAction(                      data); // Llama a la función playerAction en tu juego
+                });
+              },
+              child: GameWidget(game: game),
+            ),
+          ),
           Expanded(
               flex: 1,
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text(data),
                   const SizedBox(height: 30),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       IconButton(
-                        onPressed: () {
-                          setState(() {
-                            data = 'Piedra';
-                          });
-                        },
+                        onPressed: isTurnOver // Añade esta línea
+                            ? () {
+                                setState(() {
+                                  debugPrint("state changed to Piedra(1)");
+                                  data = 1;
+                                  game.playerAction(data);
+                                  isTurnOver = false; // Añade esta línea
+                                });
+                              }
+                            : null, // Añade esta línea
                         icon: Image.asset(
                           'assets/images/RockCard.png',
-                          width: 114.0,
-                          height: 195.0,
+                          width: ancho,
+                          height: alto,
                         ),
                         tooltip: 'Piedra',
                       ),
                       IconButton(
-                        onPressed: () {
-                          setState(() {
-                            data = 'Papel';
-                          });
-                        },
+                        onPressed: isTurnOver // Modifica esta línea
+                            ? () {
+                                setState(() {
+                                  debugPrint("state changed to Papel(2)");
+                                  data = 2;
+                                  game.playerAction(data);
+                                  isTurnOver = false; // Añade esta línea
+                                });
+                              }
+                            : null, // Añade esta línea
                         icon: Image.asset(
                           'assets/images/PaperCard.png',
-                          width: 114.0,
-                          height: 195.0,
+                          width: ancho,
+                          height: alto,
                         ),
                         tooltip: 'Papel',
                       ),
                       IconButton(
-                        onPressed: () {
-                          setState(() {
-                            data = 'Tijera';
-                          });
-                        },
+                        onPressed: isTurnOver // Modifica esta línea
+                            ? () {
+                                setState(() {
+                                  debugPrint("state changed to Tijera(3)");
+                                  data = 3;
+                                  game.playerAction(data);
+                                  isTurnOver = false; // Añade esta línea
+                                });
+                              }
+                            : null, // Añade esta línea
                         icon: Image.asset(
                           'assets/images/ScissorCard.png',
-                          width: 114.0,
-                          height: 195.0,
+                          width: ancho,
+                          height: alto,
                         ),
                         tooltip: 'Tijera',
                       ),
                     ],
-                  )
+                  ),
+                  const SizedBox(height: 30),
                 ],
               )),
         ],
